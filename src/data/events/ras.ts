288b31aaaf254555d68b43ce3ee765765977b360
Abhinav <abhinavnyps@gmail.com>
2026-08-01 09:10:52 +0530

    Add new event for AI workflows workshop using n8n
    Added a new event for an online workshop on n8n, including details such as title, description, date, venue, speakers, and media links.

--- src/data/events/ras.ts
+++ src/data/events/ras.ts
@@ -144,5 +144,30 @@ export const rasEvents: Event[] = [
       "https://sb-dataset.vercel.app/events/ras/e6/1.jpg"
     ]
   },
+    {
+    id: "ras-e7",
+    slug: "automating-the-future-ai-workflows-using-n8n",
+    title: "Automating the Future: AI Workflows Using n8n",
+    description: "IEEE RAS SBC NSSCE is organizing a free online workshop on n8n, where participants will learn to build AI-powered workflows, automate repetitive tasks, connect applications, and create real-world automation solutions without writing complex code."    
+    date: "2026-08-08T09:00:00Z",
+    venue: "Google Meet",
+    time: "07:00 PM",
+    price: "Free",
+    societyId: "ras", 
+    status: "upcoming",
+     speakers: [
+      {
+        name: "Gokul GK",
+        designation: "AI Enthusiast",
+        bio: "AI enthusiast passionate about workflow automation, AI integrations, and helping learners build practical no-code automation solutions using n8n.",
+        photo: "https://sb-dataset.vercel.app/team/default.png"
+      }
+    ],
+    banner: "https://sb-dataset.vercel.app/events/ras/e7/1.jpg",
+    tags: ["Talk Session"],
+    gallery: [
+      "https://sb-dataset.vercel.app/events/ras/e7/1.jpg"
+    ]
+  },
 
 ];
